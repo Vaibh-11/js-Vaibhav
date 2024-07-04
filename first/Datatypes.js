85310a4dad39e0220fe09b16fcf0b210334ab864
Vaibh-11 <161677222+Vaibh-11@users.noreply.github.com>
2024-07-04 10:41:41 +0000

    AllFilesInclude

---
+++ first/Datatypes.js
@@ -0,0 +1,14 @@
+"use strict";// use all js code as a new version
+// alert("hello") // we cannont use alert beacuse now we are not running js in bowser ,we are using node
+
+// number 
+// bigInt
+// string
+// boolean 
+// null   //example if we dont know the temperature we cannot say 0 beacuse 0 is a temprature se we use null
+// undefine
+//object
+// Symbol //it is used for unique identifier mainly used in react
+
+console.log(typeof null); // object
+console.log(typeof userdefine); //undefined
