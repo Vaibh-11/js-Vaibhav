4b52156baa497b8a40569065f129709e5a7e6a88
Vaibh-11 <161677222+Vaibh-11@users.noreply.github.com>
2024-07-08 05:35:09 +0000

    Heap&Stack

---
+++ first/Heap_Stack.js
@@ -0,0 +1,20 @@
+// Stack is for Premitive data 
+// Heap is for non Premitive Data
+
+let num1=5
+let num2=num1
+num1=10
+
+
+console.log(num1);
+console.log(num2);
+
+//--------------------------------------------------------------------------------------
+let name1={
+    name:"vaibhav"
+}
+let name2=name1
+name2.name="solanki"
+
+console.log(name1);
+console.log(name2);
