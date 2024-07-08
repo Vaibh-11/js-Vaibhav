25f254124fd34bb5047dd6cb2b065560b8e71f7f
Vaibh-11 <161677222+Vaibh-11@users.noreply.github.com>
2024-07-08 13:40:38 +0000

    Arrays

---
+++ first/Array.js
@@ -0,0 +1,23 @@
+let arr=new Array(1,2,3,4,5)
+console.log(arr);
+
+// arr.push(6)
+// arr.push(7)
+// console.log(arr);
+// arr.pop()
+
+
+// arr.unshift(9)
+// arr.shift()
+
+// let arr2=arr.join()
+// console.log(typeof arr);
+// console.log(typeof arr2);
+
+let narr=arr.slice(1,3)
+console.log(narr);
+console.log("A",arr);
+
+let narr2=arr.splice(1,3)
+console.log(narr);
+console.log("B",arr);
